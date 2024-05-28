f25a61752c881702d214a3e1c9a687301b04ac50
BianchiDev <ifpebianchi@gmail.com>
2024-05-28 19:31:43 -0300

    Add libs cors and express

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,17 @@
-export class AppServer {
-  private app: string
+import express from 'express' // Importação correta do express
+import 'express-async-errors' // Correção do erro de digitação
+import cors from 'cors'
 
-  constructor(info: string) {
-    this.app = info ?? 'Olá Dev'
-  }
-}
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'Olá dev!' })
+})
+
+// Correção da sintaxe da função de callback no listen
+app.listen(3000, () => {
+  console.log('Server started on port 3000!')
+})
